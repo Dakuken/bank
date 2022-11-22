3c21412f64231f8711fd138fe04b33789e0524eb
justin <justin.burnel@gmail.com>
2022-11-22 23:57:55 +0100

    wesh

--- src/app/pages/home/components/depenses/depenses.component.ts
+++ src/app/pages/home/components/depenses/depenses.component.ts
@@ -7,6 +7,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./depenses.component.scss']
 })
 export class DepensesComponent implements OnInit {
+  date = new Date();
+  hier = this.date.setUTCDate(this.date.getUTCDate() - 1)
+  tab = [
+    { name: 'Course Carrefour', price: -300, date: this.date },
+    { name: 'Achat jeux', price: -30, date: this.date },
+    { name: 'Paye du mois', price: 2713.41, date: this.date },
+    { name: 'Loyer', price: -745, date: this.hier },
+    { name: 'Électricité', price: -174, date: this.hier },
+    { name: 'Truc', price: -40, date: this.hier },
+    { name: 'Nom personalisé', price: -10, date: this.hier },
+    { name: 'Essence', price: -100, date: this.hier },
+    { name: 'Essence', price: -1000, date: this.hier },
+  ]
 
   constructor(private router: Router) { }
 
